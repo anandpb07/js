a6a230ab046de290e7713e12e5fc7394fc85a513
anandpb07 <anandbhandarkar666@gmail.com>
2025-02-19 13:08:19 +0000

    datatypes

---
+++ 01basics/01_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+/*
+let int = 2 to power of 53
+bigint = large numbers
+null = object,standalone value
+string = ""
+symbol = unique
+ 
+
+*/
+console.log(3+3)
+                       // Readibility
+console.log("Anand")
+
---
+++ 01basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = "1234"
+let accountemail = "anand@gmail.com"
+var accountpassword = "anand11"
+accountCity = "Akola"
+let accountState
+
+console.log(accountId);
+
+console.log([accountId,accountemail,accountpassword,accountCity,accountState])
