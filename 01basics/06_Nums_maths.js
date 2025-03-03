efba563e32c377ef5760eb733f4ff0960549d26d
anandpb07 <anandbhandarkar666@gmail.com>
2025-03-03 11:28:36 +0000

    maths

---
+++ 01basics/06_Nums_maths.js
@@ -0,0 +1,33 @@
+const score = 500;
+// console.log(score);
+
+const balance = new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(3));
+
+const otherNumber = 1150.78556;
+// console.log(otherNumber.toPrecision(4));
+
+const hugeNumber = 10000000;
+// console.log(hugeNumber.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-62)); // make the value to positive
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(3,2,4,5));
+// console.log(Math.max(4,68,6,4));
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10) + 1 );
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
