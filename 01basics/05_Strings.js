a5a067f6989027bc52939c9c95c7bcd31e30af3f
anandpb07 <anandbhandarkar666@gmail.com>
2025-03-03 10:57:14 +0000

    changes

---
+++ 01basics/05_Strings.js
@@ -0,0 +1,177 @@
+const name = "Anand"
+const repoCount = 8;
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello ! my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Anand')
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4).toUpperCase());
+console.log(gameName.indexOf('n'));
+
+/*
+
+anchor
+: 
+ƒ anchor()
+at
+: 
+ƒ at()
+big
+: 
+ƒ big()
+blink
+: 
+ƒ blink()
+bold
+: 
+ƒ bold()
+charAt
+: 
+ƒ charAt()
+charCodeAt
+: 
+ƒ charCodeAt()
+codePointAt
+: 
+ƒ codePointAt()
+concat
+: 
+ƒ concat()
+constructor
+: 
+ƒ String()
+endsWith
+: 
+ƒ endsWith()
+fixed
+: 
+ƒ fixed()
+fontcolor
+: 
+ƒ fontcolor()
+fontsize
+: 
+ƒ fontsize()
+includes
+: 
+ƒ includes()
+indexOf
+: 
+ƒ indexOf()
+isWellFormed
+: 
+ƒ isWellFormed()
+italics
+: 
+ƒ italics()
+lastIndexOf
+: 
+ƒ lastIndexOf()
+length
+: 
+0
+link
+: 
+ƒ link()
+localeCompare
+: 
+ƒ localeCompare()
+match
+: 
+ƒ match()
+matchAll
+: 
+ƒ matchAll()
+normalize
+: 
+ƒ normalize()
+padEnd
+: 
+ƒ padEnd()
+padStart
+: 
+ƒ padStart()
+repeat
+: 
+ƒ repeat()
+replace
+: 
+ƒ replace()
+replaceAll
+: 
+ƒ replaceAll()
+search
+: 
+ƒ search()
+slice
+: 
+ƒ slice()
+small
+: 
+ƒ small()
+split
+: 
+ƒ split()
+startsWith
+: 
+ƒ startsWith()
+strike
+: 
+ƒ strike()
+sub
+: 
+ƒ sub()
+substr
+: 
+ƒ substr()
+substring
+: 
+ƒ substring()
+sup
+: 
+ƒ sup()
+toLocaleLowerCase
+: 
+ƒ toLocaleLowerCase()
+toLocaleUpperCase
+: 
+ƒ toLocaleUpperCase()
+toLowerCase
+: 
+ƒ toLowerCase()
+toString
+: 
+ƒ toString()
+toUpperCase
+: 
+ƒ toUpperCase()
+toWellFormed
+: 
+ƒ toWellFormed()
+trim
+: 
+ƒ trim()
+trimEnd
+: 
+ƒ trimEnd()
+trimLeft
+: 
+ƒ trimStart()
+trimRight
+: 
+ƒ trimEnd()
+trimStart
+: 
+ƒ trimStart()
+valueOf
+: 
+ƒ valueOf()
+Symbol(Symbol.iterator)
+: 
+ƒ [Symbol.iterator]() 
+*/
--- 01basics/datatypes_summary.js
+++ 01basics/datatypes_summary.js
@@ -30,3 +30,6 @@ const myFunction = function(){
 console.log(typeof outsideTemp);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// Stack(Primitive) , Heap(Non-Primitive)
+
