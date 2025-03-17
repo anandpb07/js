b31b35c71bcbd628e27365362d75d71df8f44dc6
anandpb07 <anandbhandarkar666@gmail.com>
2025-03-17 23:48:32 +0000

    changes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,4 +30,38 @@ function loginUserMessage(username){
     return `${username} just logged in` 
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartPrice(...num1){   //... = rest and spread operator
+    return num1
+}
+
+// console.log(calculateCartPrice(2, 45, 100));
+
+
+const user = {
+    username: "Anand",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    }
+
+// handleObject(user)
+handleObject({
+    username: "Amm",
+    price: 799
+})
+
+const myNewArray = [200,300,400,500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+
+
+
+
