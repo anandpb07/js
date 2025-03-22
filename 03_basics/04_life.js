b1a8ea7a7f4b8a4fa8b40aa78578a2af7fc56c0c
anandpb07 <anandbhandarkar666@gmail.com>
2025-03-22 10:16:54 +0000

    changes

--- 03_basics/04_life.js
+++ 03_basics/04_life.js
@@ -1,2 +1,12 @@
-// Immediately Invoked Function Expressions (LIFE)
+// Immediately Invoked Function Expressions (IIFE)
 
+(function chain(){
+    // Named IIFE
+    console.log(`DB CONNECTED`);
+})();    //this function doesnt know when to end so we need to apply explicit semicolon
+
+
+( (name) => {
+    //IIFE with arrow funtions
+    console.log(`DB CONNECTED TWO ${name}`);
+})(`Anand`)
