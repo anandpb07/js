94efca2e2d39bc8a40d0f1db0d6c46cf66dc9065
anandpb07 <anandbhandarkar666@gmail.com>
2025-03-23 12:01:21 +0000

    changed

---
+++ 04_control_flow/one.js
@@ -0,0 +1,49 @@
+// if(condition == true){
+//  // if true then code run in the loop 
+// }
+
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if( temperature === 40 ){
+//     console.log("Less than 50");
+// } else {
+//     console.log("temperature is greater than 50")
+// }
+
+// 2 <= 2
+// < , >, <= , >=, !=, === -> checks data type tooo
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log();
+//     console.log(`User power : ${power}`);
+// }    
+// console.log(`User power : ${power}`);
+
+const balance = 1000
+
+// if (balance >  500) console.log("test");
+
+// if(balance < 500){
+//     console.log("less than");
+// }else if(balance < 750){
+//     console.log("less than 750");
+// }else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail =  true
+
+if (userLoggedIn && debitCard && 2==2){
+    console.log("Allowed to buy course");
+}
+
+if (loggedInFromEmail || loggedInFromGoogle){
+    console.log("User Logged in");
+}
