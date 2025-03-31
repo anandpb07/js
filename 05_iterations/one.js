361078a88e09d6f69e35b7a54eaacbbbe186cafb
anandpb07 <anandbhandarkar666@gmail.com>
2025-03-31 13:36:02 +0000

    changes iterations

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,7 +1,26 @@
 // for 
 
-for (let i  = 0; i  <= 10; i++) {
-    const element = i;
-    console.log(element);
+// for (let i  = 0; i  <= 10; i++) {
+//     const element = i;
+//    // console.log(element);
+// }
+
+for (let i = 1; i <= 10 ; i++) {
+        // console.log(`Outer Loop value: ${i}`);
     
+    for(let j = 1; j <= 10; j++){}
+        // console.log(`Inner Loop value: ${j} and Inner Loop: ${i}`);
+        // console.log(i + ` * `+ j + ` = ` + i*j);
+        
 }
+
+
+const myArray = ["flash","batman","Ironman"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
+
+// break and continue are opposite keyword , continue allows only one time
+     
